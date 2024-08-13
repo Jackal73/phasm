73ca180eef4369c9fed736f365e389c47987f472
Shawn Kebel <fxrender@outlook.com>
2024-08-12 22:12:31 -0500

    :sparkles::passport_control: Add `queries` for retrieving `user details` and `permissions`

---
+++ src/lib/queries.ts
@@ -0,0 +1,42 @@
+"use server";
+
+import { clerkClient, currentUser } from "@clerk/nextjs";
+import { db } from "./db";
+import {
+  Agency,
+  SubAccount,
+} from "@prisma/client";
+export const getAuthUserDetails = async () => {
+  const user = await currentUser();
+  if (!user) {
+    return;
+  }
+
+  const userData = await db.user.findUnique({
+    where: {
+      email: user.emailAddresses[0].emailAddress,
+    },
+    include: {
+      Agency: {
+        include: {
+          SidebarOption: true,
+          SubAccount: {
+            include: {
+              SidebarOption: true,
+            },
+          },
+        },
+      },
+      Permissions: true,
+    },
+  });
+  return userData;
+};
+export const getUserPermissions = async (userId: string) => {
+  const response = await db.user.findUnique({
+    where: { id: userId },
+    select: { Permissions: { include: { SubAccount: true } } },
+  });
+
+  return response;
+};
